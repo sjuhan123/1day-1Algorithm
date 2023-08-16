de51762399daf361177135d2f742690e7170dd95
sjuhan123 <sjuhan123@gmail.com>
2023-08-16 09:44:22 +0900

    ✨23.08.16 1문제 풀이 완료

---
+++ leetcode/easy/2619_ArrayPrototypeLast/solve.ts
@@ -0,0 +1,20 @@
+/**
+ * 문제출처: https://leetcode.com/problems/array-prototype-last/
+ * 시작시간: 09:30
+ * 종료시간: 09:35
+ */
+
+// declare global {
+//   interface Array<T> {
+//     last(): T | -1;
+//   }
+// }
+
+// Array.prototype.last = function () {
+//   return this.length === 0 ? -1 : this[this.length - 1];
+// };
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
